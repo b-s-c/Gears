842a9d5df7313a2c37dd45f4d916bf2c687d1d1d
Brandon Calvert <brandon.s.calvert@durham.ac.uk>
2019-01-08 21:09:12 +0000

    added toothwidth and its respective slider

--- sketch.js
+++ sketch.js
@@ -4,33 +4,42 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60); // High refresh rates like 144 and 165 break the motion slightly, but they're not recommended anyway
 
-  //toothSlider
-  toothSlider = createSlider(0, 100, 10, 1);
-  toothSlider.position(10, 10);
-  toothSlider.style('width', '300px');
+  //toothhSlider
+  toothhSlider = createSlider(0, 100, 10, 1);
+  toothhSlider.position(10, 10);
+  toothhSlider.style('width', '300px');
+
+  //toothwSlider
+  toothwSlider = createSlider(0, 100, 16, 1);
+  toothwSlider.position(10, 50);
+  toothwSlider.style('width', '300px');
 
   //speedSlider
   speedSlider = createSlider(0, 5, 1, 0.1);
-  speedSlider.position(10, 50);
+  speedSlider.position(10, 90);
   speedSlider.style('width', '300px');
 
   //g2sizeSlider
   g2sizeSlider = createSlider(10, 250, 100, 1);
-  g2sizeSlider.position(10, 90);
+  g2sizeSlider.position(10, 130);
   g2sizeSlider.style('width', '300px');
 }
 
 function draw() {
-  background(100); // Drawn here so that the gear doesn't leave a trail behind
+  background(50); // Drawn here so that the gear doesn't leave a trail behind
   gears = []; // Store our gears in an array rather than g1, g2, g3 and so on...
 
   // Text setup
-  fill(0);
+  fill(255);
   textSize(21);
 
   // tooth height
-  let toothheight = toothSlider.value() // Default toothheight is 10
-  text("Tooth height: " + toothheight, toothSlider.x * 2 + toothSlider.width, toothSlider.y + 16);
+  let toothheight = toothhSlider.value()
+  text("Tooth height: " + toothheight, toothhSlider.x * 2 + toothhSlider.width, toothhSlider.y + 16);
+
+  // tooth width
+  let toothwidth = toothwSlider.value()
+  text("Tooth width: " + toothwidth, toothwSlider.x * 2 + toothwSlider.width, toothwSlider.y + 16);  
 
   // speed
   // let speed = 0; // very useful for testing
@@ -41,21 +50,21 @@ function draw() {
   let g2size = g2sizeSlider.value()
   text("Diameter of g2: " + g2size, g2sizeSlider.x * 2 + g2sizeSlider.width, g2sizeSlider.y + 16);
 
-  // g (Gear no. 1)
-  gears.push(new Gear(mouseX, mouseY, 100, 120, 130, speed, 1, 0, toothheight, 200));
+  // gears[0]
+  gears.push(new Gear(mouseX, mouseY, 100, 120, 130, speed, 1, 0, toothheight, toothwidth, 1, 100));
   gears[0].draw();
 
-  // print(g.x) // How to get a parameter (example for quick reference)
+  // print(gears[0].x) // How to get a parameter (example for quick reference)
   
-  // g2 (Gear no.2)
-  gears.push(new Gear(mouseX + gears[0].diameter/2 + g2size/2 + toothheight, mouseY, 139, 0, 0, speed, -1, 15, toothheight, g2size)); // mouseY-100-11 for vertically above, mouseX+100+11 for horizontally right
+  // gears[1]
+  gears.push(new Gear(mouseX + gears[0].diameter/2 + g2size/2 + toothheight, mouseY, 139, 0, 0, speed, -1, 15, toothheight, toothwidth, 1, g2size)); // mouseY-100-11 for vertically above, mouseX+100+11 for horizontally right
   gears[1].draw();
 }
 
 var angle = 0 // This needs to be global, since gears can't move at different rates
 
 class Gear{
-   constructor(x, y, colR, colG, colB, speed, direction, offset, toothheight, diameter){
+   constructor(x, y, colR, colG, colB, speed, direction, offset, toothheight, toothwidth, toothscale, diameter){
       this.x = x;
       this.y = y;
       this.colR = colR;
@@ -65,6 +74,8 @@ class Gear{
       this.direction = direction; // 1 (clockwise) or -1 (counter-clockwise)
       this.offset = offset; // Angle offset, to interlock with neighbouring gears
       this.toothheight = toothheight;
+      this.toothwidth = toothwidth;
+      this.toothscale = toothscale;
       this.diameter = diameter;
    }
    draw(){
@@ -79,9 +90,14 @@ class Gear{
         translate(this.x, this.y); // Ensures that the teeth move with the gear
         rotate(radians((angle + i + this.offset) * this.direction)); // Ensures that the teeth are drawn all around the gear (i is the location ON the gear, speed is the speed that the gear is rotating)
         
-        let loY = -(this.diameter/2) + 1;
-        let hiY = loY - this.toothheight;
-        quad(-5, hiY, 5, hiY, 8, loY, -8, loY); // The drawing of the tooth (trapezium-shaped)
+
+        // Measuring out how to draw the trapezium-shaped teeth
+        let yLO = -(this.diameter/2) + 1; // +1 for making sure there's no gap between the circle and tooth
+        let yHI = yLO - this.toothheight;
+        let xHI = this.toothwidth/2
+        let xLO = xHI * (5/8) // Keeps the trapezium shape scale true to the original sketch
+
+        quad(-xLO, yHI, xLO, yHI, xHI, yLO, -xHI, yLO); // The drawing of the tooth
 
         pop(); // See push();
       }
